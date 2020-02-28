c4446d3423d5fd880ba67fdeebf21f086006ebf1
Jah Hasangjekaj <jah@campminder.com>
2020-02-27 18:49:23 -0700

    changed through master

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          I was changed on the Feature branch
+          I was changed on the master branch!
         </a>
       </header>
     </div>
